edce498f5ef1d08853d531b76b9ceeef50cfde9a
subhanmeer <banimeer@gmail.com>
2024-07-14 19:51:31 -0700

    profile page Interface

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import SignUp from "./pages/SignUp"
 import About from "./pages/About"
 import Profile from "./pages/Profile"
 import Header from "./components/Header"
-import PrivateRoute from "./components/PrivateRoute"
+  import PrivateRoute from "./components/PrivateRoute"
 
 export default function App() {
   return (
